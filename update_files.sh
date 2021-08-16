4f4aaaeae6c55e10bee79087a9532bfb6dcb711e
Joshua Gardner <102574879@student.swin.edu.au>
2021-08-16 19:16:09 +1000

    Update to update_files to auto confirm copy

--- update_files.sh
+++ update_files.sh
@@ -3,7 +3,7 @@
 curl -L https://github.com/JoshuaAndrewGardner/DevSetup/archive/refs/heads/master.zip -o master.zip
 unzip master.zip
 
-cp DevSetup-master/* .
+yes | cp DevSetup-master/* .
 
 rm -rf DevSetup-master
 rm master.zip
