065910260376b8daaf578bb21d027aab9c2e9e4f
Joshua Gardner <102574879@student.swin.edu.au>
2021-08-19 20:22:06 +1000

    Add quotes in Powershell

--- lib/CreateShortcut.ps1
+++ lib/CreateShortcut.ps1
@@ -6,7 +6,7 @@ using namespace System.IO
 $SourceFile = Resolve-Path $args[0]
 $DirName = [Path]::GetDirectoryName($SourceFile)
 $FileNameNoExtension = [Path]::GetFileNameWithoutExtension($SourceFile)
-$DestinationPath = Join-Path $DirName $FileNameNoExtension 
+$DestinationPath = Join-Path "$DirName" "$FileNameNoExtension" 
 
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("$DestinationPath.lnk")
