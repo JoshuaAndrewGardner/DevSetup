2665bf373de744b1afffc38cecca5986d7b11718
Joshua Gardner <102574879@student.swin.edu.au>
2021-08-19 20:34:24 +1000

    Add recursive flag for updating all files

--- update_files.sh
+++ update_files.sh
@@ -3,7 +3,7 @@
 curl -L https://github.com/JoshuaAndrewGardner/DevSetup/archive/refs/heads/master.zip -o master.zip
 unzip master.zip
 
-yes | cp DevSetup-master/* .
+yes | cp -r DevSetup-master/* .
 
 rm -rf DevSetup-master
 rm master.zip
